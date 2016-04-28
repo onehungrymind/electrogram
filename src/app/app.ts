8ecf60736de43f0c6dc73cf6e26a6176f3be3cf4
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-04-28 12:10:06 -0700

    02-integrating-angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -45,9 +45,6 @@ export class App {
   imageElement: HTMLImageElement;
   filters: Array<Object> = filters;
   currentFilter: string = '';
-  dialog: Electron.Dialog = window.require('remote').require('dialog');
-  fs: any = window.require('fs');
-  canvasBuffer: any = window.require('electron-canvas-to-buffer');
   showDropzone: boolean = true;
 
   constructor(
@@ -80,17 +77,6 @@ export class App {
     this.showDropzone = false;
   }
 
-  open(canvas) {
-    let self = this;
-    this.dialog.showOpenDialog( (fileNames) => {
-      if (fileNames === undefined) return;
-      let fileName = fileNames[0];
-      this.fs.readFile(fileName, 'utf-8', (err, data) => {
-        this.loadImage(canvas, fileName)
-      });
-    });
-  }
-
   setFilter(value) {
     let filterName = value.toLowerCase();
 
@@ -98,20 +84,6 @@ export class App {
       this._cs[filterName]();
     else
       this._cs.resetCanvas();
-  }
-
-  save(canvas) {
-    let self = this
-
-    this.dialog.showSaveDialog({ filters: [
-      { name: 'png', extensions: ['png'] }
-    ]}, function (fileName) {
-      if (fileName === undefined) return;
-
-      let buffer = self.canvasBuffer(canvas, 'image/png');
-
-      self.fs.writeFile(fileName, buffer, function (err) {});
-    });
   }
 
   imageLoaded(canvas, image) {
