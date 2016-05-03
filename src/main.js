686d8bf23fd56334c48f5d2bbe74a7be1e452f34
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-05-03 09:40:31 -0700

    improve app menu code + es6-ify main.js

--- src/main.js
+++ src/main.js
@@ -4,17 +4,18 @@ const electron = require('electron');
 // Module to control application life.
 const app = electron.app;
 const Menu = electron.Menu;
-var menuTemplate = require('./menuTemplate');
+let menuTemplate = require('./menuTemplate');
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
+// Allows for live-reload while developing the app
 require('electron-reload')(__dirname.replace('src', 'build'));
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow, menu;
+let mainWindow, menu, dockMenu;
 
-function createWindow () {
+let createWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
@@ -25,10 +26,9 @@ function createWindow () {
   // mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    menu.items[1].submenu.items[0].enabled = false;
-    menu.items[1].submenu.items[1].enabled = false;
-    menu.items[3].submenu.items[0].enabled = true;
+  mainWindow.on('closed', () => {
+    toggleFileTasks(false);
+    toggleNewWindowTask(true);
 
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
@@ -37,35 +37,62 @@ function createWindow () {
   });
 
   if (!menu) setMenu();
+  if (!dockMenu) setDock();
 
-  setDock();
-
-  menu.items[1].submenu.items[0].enabled = true;
-  menu.items[1].submenu.items[1].enabled = true;
-  menu.items[3].submenu.items[0].enabled = false;
+  toggleFileTasks(true);
+  toggleNewWindowTask(false);
 }
 
-function setMenu() {
-  menuTemplate[1].submenu[0].click = function () {
-    mainWindow.webContents.send('open-file');
-  }
+let toggleFileTasks = isEnabled => {
+  // The 'File' menu should only be available if there is an open window
+  menu.items
+    .find(item => item.label === 'File')
+    .submenu.items
+    .forEach(subItem => subItem.enabled = isEnabled);
+}
 
-  menuTemplate[1].submenu[1].click = function () {
-    mainWindow.webContents.send('save-file');
-  }
+let toggleNewWindowTask = isEnabled => {
+  // The 'New Window' task in the main menu and dock menu
+  // should only be available if there are no open windows
+  let newWindowMenu = menu.items
+    .find(item => item.label === 'Window')
+    .submenu.items
+    .find(subItem => subItem.label === 'New');
 
-  menuTemplate[3].submenu[0].click = function() {
-    if (!mainWindow)
-      createWindow();
-  }
+  let dockWindowMenu = dockMenu.items
+    .find(item => item.label === 'New Window');
+
+  newWindowMenu.enabled = isEnabled;
+  dockWindowMenu.enabled = isEnabled;
+}
+
+let setMenu = () => {
+  // Set custom click handlers for menu tasks
+  let fileMenu = menuTemplate
+    .find(item => item.label === 'File');
+
+  fileMenu.submenu
+    .find(item => item.label === 'Open')
+    .click = () => mainWindow.webContents.send('open-file')
+
+  fileMenu.submenu
+    .find(item => item.label === 'Save As...')
+    .click = () => mainWindow.webContents.send('save-file')
+
+  menuTemplate
+    .find(item => item.label === 'Window')
+    .submenu
+    .find(subItem => subItem.label === 'New')
+    .click = () => createWindow()
 
   menu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(menu);
 }
 
-function setDock() {
-  var dockMenu = Menu.buildFromTemplate([
-    { label: 'New Window', click:  menu.items[3].submenu.items[0].click }
+let setDock = () => {
+  // Create a 'New Window' task in the dock menu (OSX only)
+  dockMenu = Menu.buildFromTemplate([
+    { label: 'New Window', click: createWindow }
   ]);
   app.dock.setMenu(dockMenu);
 }
@@ -75,7 +102,7 @@ function setDock() {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -83,10 +110,8 @@ app.on('window-all-closed', function () {
   }
 });
 
-app.on('activate', function () {
+app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+  if (mainWindow === null) createWindow();
 });
